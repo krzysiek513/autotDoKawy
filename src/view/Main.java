604928fa9ee0a26e2fb5195cda78daafe36d0eb7
Jokr_ONE <krzysiek3155@gmail.com>
2020-04-01 17:14:48 +0200

    ulepszona fukcja main

--- src/view/Main.java
+++ src/view/Main.java
@@ -4,11 +4,18 @@ import javax.swing.*;
 
 public class Main {
 
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("maszyna");
-        frame.setContentPane(new maszyna().maszyna);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
+    public static void main(String[] args) throws ClassNotFoundException, UnsupportedLookAndFeelException, InstantiationException, IllegalAccessException {
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JFrame frame = new JFrame("maszyna");
+                frame.setContentPane(new maszyna().maszyna);
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.pack();
+                frame.setVisible(true);
+            }
+        });
     }
 }
